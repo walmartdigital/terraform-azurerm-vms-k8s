3d815cb1d9e970842a909f48d46213eb7555474f
Sebastian Diaz <sebastian.diaz@walmart.com>
2020-07-05 20:47:54 -0400

    doc(variable): add description for all variables

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "add_bastion" {
 
 variable "bastion_image_name" {
   type        = string
-  description = "azure image name to be used for bastion"
+  description = "azure image name for bastion"
   default     = "bastion-v1.0.0"
 }
 
@@ -33,13 +33,15 @@ variable "default_tags" {
 }
 
 variable "images_resource_group" {
-  type    = string
-  default = "resource_group_default"
+  type        = string
+  description = "resource group where azure images are stored"
+  default     = "resource_group_default"
 }
 
 variable "k8s_image_name" {
-  type    = string
-  default = "k8s-v1.0.0"
+  type        = string
+  description = "azure image name for kubernetes nodes"
+  default     = "k8s-v1.0.0"
 }
 
 variable "lb_ports" {
@@ -57,40 +59,48 @@ variable "lb_ports" {
 }
 
 variable "main_resource_group" {
-  type    = string
-  default = "resource_group_default"
+  type        = string
+  description = "resource group where vms will be added"
+  default     = "resource_group_default"
 }
 
 variable "manager_count" {
-  type    = string
-  default = "3"
+  type        = string
+  description = "quantity of manager nodes"
+  default     = "3"
 }
 
 variable "manager_vm_size" {
-  type    = string
-  default = "Standard_DS2_v2"
+  type        = string
+  description = "vm size for manager nodes"
+  default     = "Standard_DS2_v2"
 }
 
 variable "ssh_public_key" {
-  type = string
+  type        = string
+  description = "public ssh key to access kubernetes nodes"
 }
 
 variable "subnet_name" {
-  type    = string
-  default = "default_subnet"
+  type        = string
+  description = "name of the subnet"
+  default     = "default_subnet"
 }
 
 variable "vnet_name" {
-  type    = string
-  default = "vnet_default"
+  type        = string
+  description = "name of the vnet"
+  default     = "vnet_default"
 }
 
 variable "worker_count" {
-  type    = string
-  default = "3"
+  type        = string
+  description = "quantity of worker nodes"
+  default     = "3"
 }
 
 variable "worker_vm_size" {
-  type    = string
-  default = "Standard_DS4_v2"
+  type        = string
+  description = "vm size for worker nodes"
+  default     = "Standard_DS4_v2"
 }
