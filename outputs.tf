7d5ff46e6905aeeb66019c37ea11ef30f858ba40
Sebastian Diaz <sebastian.diaz@walmart.com>
2020-07-05 21:24:18 -0400

    feat(output): add suffix as output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,24 @@
 output "bastion_public_ip" {
-  value = azurerm_public_ip.bastion.*.ip_address
+  description = "public ip address of bastion"
+  value       = azurerm_public_ip.bastion.*.ip_address
 }
 
 output "bastion_private_ip" {
-  value = azurerm_network_interface.bastion.*.private_ip_address
-}
-
-output "worker_ips" {
-  value = join(",", azurerm_network_interface.worker.*.private_ip_address)
+  description = "private ip address of bastion"
+  value       = azurerm_network_interface.bastion.*.private_ip_address
 }
 
 output "manager_ips" {
-  value = join(",", azurerm_network_interface.manager.*.private_ip_address)
+  description = "list of ip addresses of manager nodes"
+  value       = join(",", azurerm_network_interface.manager.*.private_ip_address)
+}
+
+output "suffix" {
+  description = "random suffix generated"
+  value       = random_pet.suffix
+}
+
+output "worker_ips" {
+  description = "list of ip addresses of worker nodes"
+  value       = join(",", azurerm_network_interface.worker.*.private_ip_address)
 }
